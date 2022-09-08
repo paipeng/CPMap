479d5a494a7b504e91d5d3dcb6b16e654ecbbfa9
Pai Peng <sipaipv6@gmail.com>
2022-09-08 12:42:55 +0800

    show qml map as widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QQuickWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QHBoxLayout* hBoxLayout = new QHBoxLayout(this);
+
+    QLabel *label = new QLabel(this);
+    label->setText("test");
+
+    hBoxLayout->addWidget(label);
+
+    QQuickWidget *view = new QQuickWidget;
+    view->setSource(QUrl::fromLocalFile("main.qml"));
+    //view->show();
+    hBoxLayout->addWidget(view);
+
+    ui->centralwidget->setLayout(hBoxLayout);
 }
 
 MainWindow::~MainWindow()
